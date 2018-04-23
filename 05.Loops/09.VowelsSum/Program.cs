eff6ebc130fe645e58bcec74061bde674c829cc1
Someone Else <medical_boy_sf@yahoo.com>
2018-04-23 10:42:51 +0300

    09-10 Exercises

---
+++ 05.Loops/09.VowelsSum/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _09.VowelsSum
+{
+    class Program
+    {
+        static void Main()
+        {
+            string str = Console.ReadLine();
+            int sum = 0;
+            int val = 0;
+           
+            
+
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                switch (str[i])
+                {
+                    case 'a':
+                        val = 1;
+                        break;
+                    case 'o':
+                        val = 4;
+                        break;
+
+                    case 'u':
+                        val = 5;
+                        break;
+
+                    case 'i':
+                        val = 3;
+                        break;
+
+                    case 'e':
+                        val = 2;
+                        break;
+                    default:
+                        val = 0;
+                        break;
+                }
+                if (str[i] == 'a' || str[i] == 'o' || str[i] == 'e' || str[i] == 'i' || str[i] == 'u')
+                {
+                    sum += val;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ 05.Loops/10.HalfSum/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _10.HalfSum
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int maxNumber = int.MinValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                sum += number;
+                if (number > maxNumber)
+                {
+                    maxNumber = number;
+                }
+            }
+
+            if (sum - maxNumber == maxNumber)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {maxNumber}");
+            }
+
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(maxNumber - (sum - maxNumber))}");
+            }
+        }
+    }
+}
