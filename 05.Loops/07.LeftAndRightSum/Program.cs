11521210af87cfddaa2a3e9975d5e679d417328c
Someone Else <medical_boy_sf@yahoo.com>
2018-04-23 09:58:12 +0300

    07-08

---
+++ 05.Loops/07.LeftAndRightSum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _07.LeftAndRightSum
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum1 = 0;
+            int sum2 = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                
+                    sum1 += number;
+            }
+
+            for (int j = 0; j < n; j++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                sum2 += number;
+            }
+
+            if (sum1 == sum2)
+            {
+                Console.WriteLine($"Yes, sum = {sum1}");
+            }
+
+            else
+            {
+                Console.WriteLine($"No, diff = {Math.Abs(sum1 - sum2)}");
+            }
+        }
+    }
+}
