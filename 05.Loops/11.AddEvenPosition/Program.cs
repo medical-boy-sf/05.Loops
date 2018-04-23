3da57ad12d4454d91a97123599fd30c11e3caf89
Someone Else <medical_boy_sf@yahoo.com>
2018-04-23 21:46:20 +0300

    11. Add Even Position

---
+++ 05.Loops/11.AddEvenPosition/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace _11.AddEvenPosition
+{
+    class Program
+    {
+        static void Main()
+        {
+            double n = double.Parse(Console.ReadLine());
+            double oddSum = 0;
+            double oddMax = double.MinValue;
+            double oddMin = double.MaxValue;
+            double evenSum = 0;
+            double evenMax = double.MinValue;
+            double evenMin = double.MaxValue;
+
+            for (int i = 1; i <= n; i++)
+            {
+                double number = double.Parse(Console.ReadLine());
+                if (i % 2 == 0)
+                {
+                    evenSum += number;
+
+                    if (number > evenMax)
+                    {
+                        evenMax = number;
+                    }
+
+                    if (number < evenMin)
+                    {
+                        evenMin = number;
+                    }
+                }
+
+                else
+                {
+                    oddSum += number;
+
+                    if (number > oddMax)
+                    {
+                        oddMax = number;
+                    }
+
+                    if (number < oddMin)
+                    {
+                        oddMin = number;
+                    }
+                }
+            }
+
+            if (evenSum == 0 && oddSum == 0)
+            {
+                Console.WriteLine($"OddSum=0,");
+                Console.WriteLine($"OddMin=No,");
+                Console.WriteLine($"OddMax=No,");
+                Console.WriteLine($"EvenSum=0,");
+                Console.WriteLine($"EvenMin=No,");
+                Console.WriteLine($"EvenMax=No");
+            }
+
+            else if (oddSum == 0)
+            {
+                Console.WriteLine($"OddSum=0,");
+                Console.WriteLine($"OddMin=No,");
+                Console.WriteLine($"OddMax=No,");
+                Console.WriteLine($"EvenSum={evenSum},");
+                Console.WriteLine($"EvenMin={evenMin},");
+                Console.WriteLine($"EvenMax={evenMax}");
+            }
+
+            else if (evenSum == 0)
+            {
+
+                Console.WriteLine($"OddSum={oddSum},");
+                Console.WriteLine($"OddMin={oddMin},");
+                Console.WriteLine($"OddMax={oddMax},");
+                Console.WriteLine($"EvenSum=0,");
+                Console.WriteLine($"EvenMin=No,");
+                Console.WriteLine($"EvenMax=No");
+            }
+
+
+            else
+            {
+                Console.WriteLine($"OddSum={oddSum},");
+                Console.WriteLine($"OddMin={oddMin},");
+                Console.WriteLine($"OddMax={oddMax},");
+                Console.WriteLine($"EvenSum={evenSum},");
+                Console.WriteLine($"EvenMin={evenMin},");
+                Console.WriteLine($"EvenMax={evenMax}");
+            }
+        }
+    }
+}
